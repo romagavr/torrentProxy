46bfa8c379bf799caee5c6afbec7e7e83aeb152c
= <romagavr@hotmail.com>
2020-03-30 01:01:39 +0300

    Bug fixing

--- main.go
+++ main.go
@@ -2,85 +2,24 @@ package main
 
 import (
     "bytes"
-    "encoding/binary"
     "fmt"
     _ "fmt"
     "io"
     "io/ioutil"
-    "math/rand"
     "net"
-    "net/http"
     _ "net/http"
-    "net/url"
     "strconv"
     "time"
     "torrentProxy/src"
 )
 
-type Peer struct {
-    Ip net.IP
-    Port uint16
-}
-
-type TorrentResponse struct {
-    Interval int
-    MinInterval int
-    Peers []Peer
-}
-
 func main()  {
     // Parse bencodeed File
+    data, err := ioutil.ReadFile("/home/roman/golang/testfiles/9.torrent")
     bencode := src.BencodeTorrent{}
-    bencode = src.GetBencodeStruct("/home/roman/golang/testfiles/9.torrent")
-    // Create random PeerId
-    peerId := make([]byte, 20)
-    letters := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    for i := range peerId {
-        peerId[i] = letters[rand.Intn(len(letters))]
-    }
-    // Send request to server
-    base, err := url.Parse(bencode.Announce)
-    if err != nil {
-       fmt.Println("error")
-       return
-    }
-    params := url.Values{
-        "info_hash":  []string{string(bencode.InfoHash[:])},
-        "peer_id":    []string{string(peerId[:])},
-        "port":       []string{strconv.Itoa(6969)},
-        "uploaded":   []string{"0"},
-        "downloaded": []string{"0"},
-        "compact":    []string{"1"},
-        "left":       []string{strconv.Itoa(bencode.Info.Length)},
-    }
-    base.RawQuery = params.Encode()
-    resp, err := http.Get(base.String())
-    //defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    ioutil.WriteFile("/home/roman/golang/testfiles/4.torrent", body, 0644)
-    serverResp, err := ioutil.ReadFile("/home/roman/golang/testfiles/4.torrent")
-    d := src.New(serverResp)
-    t := d.Decode()
-    response := TorrentResponse{}
-    for i, val := range t.(map[string]interface{}) {
-        switch i {
-        case "interval":
-            response.Interval = val.(int)
-        case "min interval":
-            response.MinInterval = val.(int)
-        case "peers":
-            byteStr := []byte(val.(string))
-            peersCount := len(byteStr)/6
-            for k:=0;k < peersCount ; k++  {
-               offset := k * 6
-               response.Peers = append(
-                   response.Peers,
-                   Peer{net.IP(byteStr[offset : offset + 4]), binary.BigEndian.Uint16(byteStr[offset+4 : offset+6])})
-            }
+    bencode.GetBencodeStruct(data)
 
-        }
-    }
-    //fmt.Printf("%s", response.Peers[0].Port)
+    response := bencode.GetTorrentRequest()
 
     //Format Bittorrent handshake
     var handShake []byte
@@ -90,7 +29,7 @@ func main()  {
     count += copy(handShake[count:], "BitTorrent protocol")
     count += copy(handShake[count:], make([]byte, 8))
     count += copy(handShake[count:], bencode.InfoHash[:])
-    count += copy(handShake[count:], peerId[:])
+    count += copy(handShake[count:], bencode.PeerId[:])
     //fmt.Println(handShake)
 
     //Connection to Peer
--- src/src.go
+++ src/src.go
@@ -1,10 +1,15 @@
 package src
 
 import (
+    "bytes"
     "crypto/sha1"
+    "encoding/binary"
     "fmt"
     "io/ioutil"
-    "bytes"
+    "math/rand"
+    "net"
+    "net/http"
+    "net/url"
     "strconv"
 )
 
@@ -119,22 +124,19 @@ type BencodeTorrent struct {
     AnnounceList []string
     InfoHash [20]uint8
     Info bencodeInfo
+    PeerId [20]byte
 }
 
-func GetBencodeStruct(torrentPath string) BencodeTorrent {
-    dat, err := ioutil.ReadFile(torrentPath)
-    check(err)
-    //fmt.Printf("File contents: %s", dat)
-    d := New([]byte(dat))
-    t := d.Decode()
-    bencode := BencodeTorrent{}
+func (bencode *BencodeTorrent) GetBencodeStruct(data []byte) {
+    t := New(data).Decode()
+    bencode.PeerId = CreatePeerId()
     for i, val := range t.(map[string]interface{}) {
         switch i {
             case "announce":
                 bencode.Announce = val.(string)
             case "announce-list":
                 for _, value := range val.([]interface{}) {
-                   for _, vll := range value.([]interface{}){
+                   for _, vll := range value.([]interface{}) {
                     bencode.AnnounceList = append(bencode.AnnounceList, vll.(string))
                     }
                 }
@@ -143,29 +145,80 @@ func GetBencodeStruct(torrentPath string) BencodeTorrent {
             case "info":
                 for j, value := range val.(map[string]interface{}) {                  
                     switch j {
-                    case "pieces":
-                        hash := make([]byte,20)
-                        for k, r := range value.(string) {
-                           hash = append(hash, byte(r))
-                           if k % 19 == 0 {
-                               //fmt.Println(len(hash))
-                               bencode.Info.Pieces = append(bencode.Info.Pieces, hash)
-                               hash = nil
-                                hash = make([]byte,20)
-                        }
-                        }
-                    case "piece length":
-                        bencode.Info.PieceLength = value.(int)
-                    case "length":
-                        bencode.Info.Length = value.(int)
-                    case "name":
-                        bencode.Info.Name = value.(string)
-                    }    
-        }}
-        //fmt.Println(i)
-        //fmt.Println(val) 
+                        case "pieces":
+                            byts := []byte(value.(string))
+                            bencode.Info.Pieces = make([][]byte, len(byts)/20)
+                            for k:=0; k < len(byts)/20; k++ {
+                               copy(bencode.Info.Pieces[k], byts[k*20:k*20+20])
+                            }
+                        case "piece length":
+                            bencode.Info.PieceLength = value.(int)
+                        case "length":
+                            bencode.Info.Length = value.(int)
+                        case "name":
+                            bencode.Info.Name = value.(string)
+                    }
+                }
+        }
     }
-    //fmt.Println(len(bencode.Info.Pieces))
-    //fmt.Printf("%s", t["info"].(map[string]interface{}))\
-    return bencode
+}
+
+func CreatePeerId() [20]byte {
+    var peerId [20]byte
+    letters := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    for i := range peerId {
+        peerId[i] = letters[rand.Intn(len(letters))]
+    }
+    return peerId
+}
+
+type Peer struct {
+    Ip net.IP
+    Port uint16
+}
+
+type TorrentResponse struct {
+    Interval int
+    MinInterval int
+    Peers []Peer
+}
+
+func (bencode *BencodeTorrent) GetTorrentRequest() TorrentResponse {
+    base, err := url.Parse(bencode.Announce)
+    if err != nil {
+        fmt.Println("error")
+    }
+    params := url.Values{
+        "info_hash":  []string{string(bencode.InfoHash[:])},
+        "peer_id":    []string{string(bencode.PeerId[:])},
+        "port":       []string{strconv.Itoa(6969)},
+        "uploaded":   []string{"0"},
+        "downloaded": []string{"0"},
+        "compact":    []string{"1"},
+        "left":       []string{strconv.Itoa(bencode.Info.Length)},
+    }
+    base.RawQuery = params.Encode()
+    resp, err := http.Get(base.String())
+    //defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    return parseTorrentResponse(New(body).Decode())
+}
+
+func parseTorrentResponse(data interface{}) TorrentResponse {
+    response := TorrentResponse{}
+    for i, val := range data.(map[string]interface{}) {
+        switch i {
+            case "interval":
+                response.Interval = val.(int)
+            case "min interval":
+                response.MinInterval = val.(int)
+            case "peers":
+                byts := []byte(val.(string))
+                response.Peers = make([]Peer, len(byts)/6)
+                for k:=0; k < len(byts)/6; k++ {
+                    response.Peers[k] = Peer{net.IP(byts[6*k:6*k+4]), binary.BigEndian.Uint16(byts[6*k+4 : 6*k+6])}
+                }
+        }
+    }
+    return response
 }
