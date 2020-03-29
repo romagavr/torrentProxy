0aec4c66cf28d7b3c6c2cc7ad4e2a60411e6c7be
= <romagavr@hotmail.com>
2020-03-29 18:52:38 +0300

    Added handshake

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+    "bytes"
     "encoding/binary"
     "fmt"
     _ "fmt"
+    "io"
     "io/ioutil"
     "math/rand"
     "net"
@@ -11,6 +13,7 @@ import (
     _ "net/http"
     "net/url"
     "strconv"
+    "time"
     "torrentProxy/src"
 )
 
@@ -26,18 +29,20 @@ type TorrentResponse struct {
 }
 
 func main()  {
+    // Parse bencodeed File
     bencode := src.BencodeTorrent{}
-    bencode = src.GetBencodeStruct("/home/roman/golang/testfiles/2.torrent")
-    //fmt.Println(bencode.InfoHash)
-    base, err := url.Parse(bencode.Announce)
-    if err != nil {
-       fmt.Println("error")
-       return
-    }
+    bencode = src.GetBencodeStruct("/home/roman/golang/testfiles/9.torrent")
+    // Create random PeerId
     peerId := make([]byte, 20)
     letters := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
     for i := range peerId {
         peerId[i] = letters[rand.Intn(len(letters))]
+    }
+    // Send request to server
+    base, err := url.Parse(bencode.Announce)
+    if err != nil {
+       fmt.Println("error")
+       return
     }
     params := url.Values{
         "info_hash":  []string{string(bencode.InfoHash[:])},
@@ -75,5 +80,48 @@ func main()  {
 
         }
     }
-    fmt.Printf("%s", response.Peers[0].Port)
+    //fmt.Printf("%s", response.Peers[0].Port)
+
+    //Format Bittorrent handshake
+    var handShake []byte
+    handShake = make([]byte, 68)
+    handShake[0] = byte(len("BitTorrent protocol"))
+    count := 1
+    count += copy(handShake[count:], "BitTorrent protocol")
+    count += copy(handShake[count:], make([]byte, 8))
+    count += copy(handShake[count:], bencode.InfoHash[:])
+    count += copy(handShake[count:], peerId[:])
+    //fmt.Println(handShake)
+
+    //Connection to Peer
+    fmt.Printf("%s", response.Peers[6])
+    dkf  := response.Peers[3].Ip.String() + ":" + strconv.Itoa(int(response.Peers[3].Port))
+    //fmt.Println(dkf)
+    conn, err := net.DialTimeout("tcp", dkf, 10 * time.Second)
+    if err != nil {
+        //fmt.Println("sdkdsdf")
+       fmt.Println(err)
+    }
+
+    fmt.Println("Written:" + string(handShake))
+    _, err = conn.Write(handShake)
+    fmt.Println("Written:" + string(handShake))
+    if err != nil {
+       fmt.Println("Peer writting error")
+    }
+    fmt.Println("2323")
+    hhh := make([]byte, 1)
+        _, err = io.ReadFull(conn, hhh)
+        if err != nil {
+            fmt.Println(err)
+        }
+        fmt.Println(hhh)
+    handshakeBuf := make([]byte, 48+int(hhh[0]))
+    _, err = io.ReadFull(conn, handshakeBuf)
+    fmt.Println(string(handshakeBuf))
+    hash := handshakeBuf[27:47]
+    fmt.Printf("%s", hash)
+    if bytes.Compare(hash, bencode.InfoHash[:]) == 0 {
+        fmt.Println("OK!!")
+    }
 }
