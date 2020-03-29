53301a8dd3f2f7a52f627e0c32d316455ca57004
= <romagavr@hotmail.com>
2020-03-29 12:05:58 +0300

    Add torrent response parsing

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+    "encoding/binary"
     "fmt"
     _ "fmt"
     "io/ioutil"
     "math/rand"
+    "net"
     "net/http"
+    _ "net/http"
     "net/url"
     "strconv"
     "torrentProxy/src"
 )
+
+type Peer struct {
+    Ip net.IP
+    Port uint16
+}
+
+type TorrentResponse struct {
+    Interval int
+    MinInterval int
+    Peers []Peer
+}
 
 func main()  {
     bencode := src.BencodeTorrent{}
@@ -36,8 +50,30 @@ func main()  {
     }
     base.RawQuery = params.Encode()
     resp, err := http.Get(base.String())
-    defer resp.Body.Close()
+    //defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
+    ioutil.WriteFile("/home/roman/golang/testfiles/4.torrent", body, 0644)
+    serverResp, err := ioutil.ReadFile("/home/roman/golang/testfiles/4.torrent")
+    d := src.New(serverResp)
+    t := d.Decode()
+    response := TorrentResponse{}
+    for i, val := range t.(map[string]interface{}) {
+        switch i {
+        case "interval":
+            response.Interval = val.(int)
+        case "min interval":
+            response.MinInterval = val.(int)
+        case "peers":
+            byteStr := []byte(val.(string))
+            peersCount := len(byteStr)/6
+            for k:=0;k < peersCount ; k++  {
+               offset := k * 6
+               response.Peers = append(
+                   response.Peers,
+                   Peer{net.IP(byteStr[offset : offset + 4]), binary.BigEndian.Uint16(byteStr[offset+4 : offset+6])})
+            }
 
-    fmt.Printf("%s", body)
+        }
+    }
+    fmt.Printf("%s", response.Peers[0].Port)
 }
